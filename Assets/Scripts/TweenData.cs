38e774c5a03da4be66cf41a3852e583bfee83009
Muharrem Sait ÇOKTAŞ <coktasmuharremsait@gmail.com>
2021-12-19 11:15:22 +0300

    Initial Commit

---
+++ Assets/Scripts/TweenData.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+[System.Serializable]
+public class TweenData
+{
+	public enum TweenType { Move, Rotate, Scale }
+
+	public TweenType tweenType;
+	public Transform objectToTween;
+	public Vector3 positionOffset;
+	public float duration = 1f;
+	public Vector3 targetRotation;
+	public float targetScale = 1f;
+	public bool join = false;
+
+
+	public Tween GetTween(){
+		Tween tween = null;
+
+		switch(tweenType){
+			case TweenType.Move:
+				Vector3 targetPosition = objectToTween.transform.position + positionOffset;
+				tween = objectToTween.DOMove(targetPosition, duration);
+				break;
+			case TweenType.Rotate:
+				tween = objectToTween.DORotate(targetRotation, duration);
+				break;
+			case TweenType.Scale:
+				tween = objectToTween.DOScale(targetScale, duration);
+				break;
+			default:
+				break;		
+		}
+
+		return tween;
+	}
+}
---
+++ Assets/Scripts/TweenSequencer.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class TweenSequencer : MonoBehaviour
+{
+	public TweenData[] tweenDataArray;
+
+	Sequence mySequence;
+
+	private bool sequenceTriggered;
+
+	void Awake(){
+		mySequence = DOTween.Sequence();
+		BuildSequence();
+	}
+
+	void BuildSequence(){
+		//Take control for you
+		mySequence.Pause();
+		mySequence.SetAutoKill(false);
+
+		for(int i = 0; i < tweenDataArray.Length; i++){
+			if(tweenDataArray[i].join){
+				mySequence.Join(tweenDataArray[i].GetTween()); //meaining we're gonna play concurrently
+			}else{
+				mySequence.Append(tweenDataArray[i].GetTween()); //just builds a sequential sequence of tweens
+				//so those movements are gonna happen one after the other then
+			}
+		}
+	}
+
+
+	[ContextMenu("Start Sequence")]
+	public void StartSequence(){
+		Debug.Log("Start sequence on " + gameObject.name);
+
+		if(!sequenceTriggered){
+			mySequence.PlayForward();
+		}else{
+			mySequence.PlayBackwards();
+		}
+
+		sequenceTriggered = !sequenceTriggered;
+	}
+}
